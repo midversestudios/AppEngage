4dda631fedfa024f7b1525e3fa386e3524afa7e7
Sebastian Santacroce <sebastian>
2014-03-17 20:23:23 -0700

    Unity SDK version 1.1.2

---
+++ Unity SDK/AppEngageUnitySampleApp/Assets/Resources/MidversePluginHandler.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MidversePluginHandler : MonoBehaviour {
+	static bool initialized = false;
+	SampleAppScript instance;
+	void Start () {
+		DontDestroyOnLoad(this);
+		instance=GameObject.Find("Main Camera").GetComponent("SampleAppScript") as SampleAppScript;
+	}
+	
+ 
+	
+	
+	void OnEnable()
+	{
+		Debug.Log ("OnEnable");
+		MidversePluginManager.nGageRewardEvent += nGageRewardEvent;
+		MidversePluginManager.nGageInterstitialEvent += nGageInterstitialEvent;
+		
+	}
+	
+	void OnDisable()
+	{
+		Debug.Log ("OnDisable");
+		MidversePluginManager.nGageRewardEvent -= nGageRewardEvent;
+		MidversePluginManager.nGageInterstitialEvent -= nGageInterstitialEvent;
+	}
+	
+	
+	void nGageRewardEvent( int reward, string currencyClaimToken )
+	{
+		instance.reward = reward;
+		Debug.Log( "nGageRewardEvent reward="+reward+" currencyClaimToken="+currencyClaimToken );
+
+	}
+	
+	void nGageInterstitialEvent( bool displayed, string errorCode )
+	{
+		//Debug.LogError( "displayed="+displayed+" errorCode="+errorCode );
+	}
+	
+	
+}
